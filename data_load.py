cdc4d416895650638ab12d650a0bff98525f32b1
Xiang Li <xli62@student.gsu.edu>
2021-01-04 12:21:19 -0500

    path of dataset

--- data_load.py
+++ data_load.py
@@ -10,27 +10,27 @@ def load_data(para, dataset):
     """
     if dataset == 0:
         x = np.load(
-            '/home/xli62/brain/brain_l0/data_3folder/xb_t_{}_{}.npy'.format(para['data_fold'], para['data_seed']))
+            '/home/xli62/brain_new/data/dataset/xb_t_{}_{}.npy'.format(para['data_fold'], para['data_seed']))
         y = np.load(
-            '/home/xli62/brain/brain_l0/data_3folder/yb_t_{}_{}.npy'.format(para['data_fold'], para['data_seed']))
+            '/home/xli62/brain_new/data/dataset/yb_t_{}_{}.npy'.format(para['data_fold'], para['data_seed']))
         y = y - 1
         y = y.astype(np.long)
     elif dataset == 1:
         x = np.load(
-            '/home/xli62/brain/brain_l0/data_3folder/xb_v_{}_{}.npy'.format(para['data_fold'], para['data_seed']))
+            '/home/xli62/brain_new/data/dataset/xb_v_{}_{}.npy'.format(para['data_fold'], para['data_seed']))
         y = np.load(
-            '/home/xli62/brain/brain_l0/data_3folder/yb_v_{}_{}.npy'.format(para['data_fold'], para['data_seed']))
+            '/home/xli62/brain_new/data/dataset/yb_v_{}_{}.npy'.format(para['data_fold'], para['data_seed']))
         y = y - 1
         y = y.astype(np.long)
     elif dataset == 2:
-        x = np.load('/home/xli62/brain/val_dataset_SZonly/new_val_data_clean_0_gg6.npy')
-        y = np.load('/home/xli62/brain/val_dataset_SZonly/new_val_label_clean_0_gg6.npy')
+        x = np.load('/home/xli62/brain_new/data/dataset/top_data.npy')
+        y = np.load('/home/xli62/brain_new/data/dataset/top_label.npy')
     elif dataset == 3:
-        x = np.load('/home/xli62/brain/val_dataset_SZonly/new_val_data_clean_0_gg7.npy')
-        y = np.load('/home/xli62/brain/val_dataset_SZonly/new_val_label_clean_0_gg7.npy')
+        x = np.load('/home/xli62/brain_new/data/dataset/hubin_data.npy')
+        y = np.load('/home/xli62/brain_new/data/dataset/hubin_label.npy')
     elif dataset == 4:
-        x = np.load('/home/xli62/brain/val_dataset_BSNIP/new_val_data_clean_0.npy')
-        y = np.load('/home/xli62/brain/val_dataset_BSNIP/new_val_label_clean_0.npy')
+        x = np.load('/home/xli62/brain_new/data/dataset/bsnip_data.npy')
+        y = np.load('/home/xli62/brain_new/data/dataset/bsnip_label.npy')
     x = x.astype(np.float32)
     # reshape and to tensor
     x = torch.tensor(x)
